4047e372a363fa75e0149bdddc661a1368d27fbe
WidgetA <421413836@qq.com>
2019-05-04 04:45:15 +0800

    CSS
    CSS update

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoieWlzb24xMSIsImEiOiJjanY3cDh6MHYwMzF6M3lvM3hjZm9xYW9hIn0.ZRrZ6XXMSTqZmZjo9ybmsg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
