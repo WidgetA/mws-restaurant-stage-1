92a000958b930bf0a4ec2b4ebb02768a0e6e126b
WidgetA <421413836@qq.com>
2019-05-11 00:55:18 +0800

    Update service_worker.js

--- js/service_worker.js
+++ js/service_worker.js
@@ -1,5 +1,9 @@
 var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [ '/', '/img/', /js/]; 
+var urlsToCache = ['/index.html', '/restaurant/html',
+		'/css/style.css', '/data/restaurant.json',
+		'/img/1.jpg', '/img/2.jpg', '/img/3.jpg', '/img/4.jpg', '/img/5.jpg',
+		'/img/6.jpg', '/img/7.jpg', '/img/8.jpg', '/img/9.jpg', '/img/10.jpg',
+		'/js/dbhelper.js', '/js/main.js', '/js/restaurant_info.js', '/js/service_worker.js']; 
 
 self.addEventListener('install', function(event) { 
    // Perform install steps
